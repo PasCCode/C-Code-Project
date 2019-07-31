cb03cd7e0449aebf3772a22561c5a78e07216da5
PasCCode <pascal.hasko@gmail.com>
2019-07-31 15:45:57 +0200

    Überschrift als Kommentar hinzugefügt

---
+++ Sum-Function.c
@@ -0,0 +1,38 @@
+//Hier eine Summenfunktion in C//
+
+#include<stdio.h>
+
+#include<conio.h>
+
+void main()
+{
+
+ int a, b, sum;
+
+
+ clrscr();
+
+ printf("Hallo Benutzer, bitte gib zwei Zahlen für a und b ein\n");
+
+ scanf("%d %d",&a &b);
+
+ sum = a + b;
+
+ printf("Die Summe ist %d",sum);
+
+ getch();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
