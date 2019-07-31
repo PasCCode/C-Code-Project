14179c8e0c4bda40f7949e7c9f4f36e9fcece1d3
PasCCode <pascal.hasko@gmail.com>
2019-07-31 11:05:05 +0200

    Erstes Abspeichern der C-Addition

---
+++ Sum-Function.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+#include<conio.h>
+
+void main()
+{
+
+ int a, b, sum;
+
+
+ clrscr();
+
+ printf("Hallo Benutzer, bitte gib zwei Zahlen für a und b ein\n");
+
+ scanf("%d %d ",&a &b);
+
+ sum = a + b;
+
+ printf("Die Summe ist %d",sum);
+
+ getch();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
