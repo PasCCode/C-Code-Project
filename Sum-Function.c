0b3ecef0a614f732718708cc5335ae7ab4629960
PasCCode <pascal.hasko@gmail.com>
2019-08-01 16:46:27 +0200

    Conio-Funktion auskommentiert

--- Sum-Function.c
+++ Sum-Function.c
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
 //Hier eine Summenfunktion in C//
 
-=======
->>>>>>> 14179c8e0c4bda40f7949e7c9f4f36e9fcece1d3
 #include<stdio.h>
 
-#include<conio.h>
+//#include<conio.h>//
 
 void main()
 {
@@ -17,11 +14,7 @@ void main()
 
  printf("Hallo Benutzer, bitte gib zwei Zahlen für a und b ein\n");
 
-<<<<<<< HEAD
  scanf("%d %d",&a &b);
-=======
- scanf("%d %d ",&a &b);
->>>>>>> 14179c8e0c4bda40f7949e7c9f4f36e9fcece1d3
 
  sum = a + b;
 
@@ -42,8 +35,4 @@ void main()
 
 
 
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 14179c8e0c4bda40f7949e7c9f4f36e9fcece1d3
